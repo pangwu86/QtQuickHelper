fa21c12074a7df517ff3b65a089f0989b7d3edd3
pangwu86 <pangwu86@gmail.com>
2013-06-04 09:34:20 +0800

    ..

--- qml/plugins/js/util.js
+++ qml/plugins/js/util.js
@@ -2,7 +2,26 @@ var _log_nm_ = "qml"
 
 var z = {
     log: function (nm, text) {
-        console.debug((z.isBlank(nm) ? _log_nm_ : nm) + " : " + text)
+        console.debug(z.now() + " " + (z.isBlank(
+                                           nm) ? _log_nm_ : nm) + " : " + text)
+    },
+    now: function (needMs) {
+        // 返回yyyy-MM-dd_hh:mm:ss:sss格式化日期时间字符串
+        var n = new Date()
+        var fnow = n.getFullYear() + "-" + z.numberFormat(
+                    n.getMonth() + 1, 2) + "-" + z.numberFormat(n.getDate(), 2)
+        var snow = z.numberFormat(n.getHours(), 2) + ":" + z.numberFormat(
+                    n.getMinutes(), 2) + ":" + z.numberFormat(n.getSeconds(), 2)
+        return fnow + "_" + snow + (needMs ? ":" + n.getMilliseconds() : "")
+    },
+    numberFormat: function (num, length) {
+        var nstr = num + ""
+        if (nstr.length < length) {
+            for (var i = nstr.length; i < length; i++) {
+                nstr = "0" + nstr
+            }
+        }
+        return nstr
     },
     isBlank: function (str) {
         if (str == undefined || str == null) {
